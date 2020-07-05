72a2a9beae09f31841f204726f0ebd9c793739ac
mohammadkm0910 <mohammadkm0910@gmail.com>
2020-07-05 14:54:58 +0430

    add contorller post

---
+++ News Technology Web Site/Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace News_Technology_Web_Site.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult IBMstoppedFaceRecognitionTechnology()
+        {
+            return View();
+        }
+        public ActionResult MicrosoftRemoveWindows10FreshStart()
+        {
+            return View();
+        }
+    }
+}
